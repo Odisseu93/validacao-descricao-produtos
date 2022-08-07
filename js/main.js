0968721f4096bd97f65106dce96f9a9e0ca25696
Ulisses Silverio <ulissessuporteti01@gmail.com>
2022-08-07 16:51:59 -0300

    alterado o arquivos termos-proibidos para json

--- js/main.js
+++ js/main.js
@@ -4,46 +4,64 @@ const viewErros = document.querySelector("#viewErros");
 const btnVer = document.querySelector('#btnVer');
 const btnLimpar = document.querySelector('#btnLimpar');
 const feedbackDescricao = document.querySelector('#feedBDescricao');
+const url = "./js/termos-proibidos.json";
+
 
 
 descricao.classList.add('estah-visivel')
 
+
 container.addEventListener('input', (e) => {
-    let descricaoInnerHTML = descricao.innerHTML;
-    const textoInput = e.target.innerText;
 
-    if (descricao.innerText != "") {
-        let erros = [];
 
-        //testa a descrição
-        termos_proibida_array.some(termo => {
-            const reg = new RegExp(termo, 'i');
-            if (reg.test(textoInput)) {
-                erros.push(termo);
-                console.log(descricaoInnerHTML)
-                descricaoInnerHTML = descricaoInnerHTML.replace(termo, `<span  class="palavras-proibidas">${termo}</span>`);
-                console.log(descricaoInnerHTML)
-                update(descricaoInnerHTML);
-                
-            };
-        });
-        
+    fetch(url) // requisição
+        .then(response => {
+            return response.json()
+        })
+        .then(dados => {
+            const termosProibidos = dados.termo;
+            console.log(termosProibidos);
 
-        //caso não tenha erro
-        if (erros.length === 0) {
-            feedbackDescricao.innerText = 'Tudo Certo!';
-            feedbackDescricao.classList.add('allrigth');
-        }
-        else {  // caso tenha erro
-            feedbackDescricao.classList.remove('allrigth');
-            feedbackDescricao.classList.add('erro');
+            let descricaoInnerHTML = descricao.innerHTML;
+            const textoInput = e.target.innerText;
 
-            feedbackDescricao.innerText = `palavras proibidas: "${erros}"`;
+            if (descricao.innerText != "") {
+                let erros = [];
 
-        }
-    } else { //reset
-        limpar();
-    }
+                //testa a descrição
+                termosProibidos.some(termo => {
+                    const reg = new RegExp(termo, 'i');
+                    if (reg.test(textoInput)) {
+                        erros.push(termo);
+                        console.log(descricaoInnerHTML);
+                        descricaoInnerHTML = descricaoInnerHTML.replace(termo, `<span  class="palavras-proibidas">${termo}</span>`);
+                        console.log(descricaoInnerHTML)
+                        update(descricaoInnerHTML);
+
+                    };
+                });
+
+                //caso não tenha erro
+                if (erros.length === 0) {
+                    feedbackDescricao.innerText = 'Tudo Certo!';
+                    feedbackDescricao.classList.add('allrigth');
+                }
+                else {  // caso tenha erro
+                    feedbackDescricao.classList.remove('allrigth');
+                    feedbackDescricao.classList.add('erro');
+
+                    feedbackDescricao.innerText = `palavras proibidas: "${erros}"`;
+
+                }
+            } else { //reset
+                limpar();
+            }
+
+        })
+        .catch((e) => {
+            throw Error(e)
+        })
+
 })
 
 
@@ -62,10 +80,11 @@ const limpar = () => {
     feedbackDescricao.classList.remove('allrigth');
     feedbackDescricao.classList.remove('erro');
     feedbackDescricao.innerText = '';
-} 
+}
 
-function update(descricaoInnerHTML){
+function update(descricaoInnerHTML) {
     viewErros.innerHTML = descricaoInnerHTML;
 }
+
 
 
