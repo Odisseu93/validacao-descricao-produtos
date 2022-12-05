ec7eee5664d46e335614fc959c611e4ca8d276a7
Ulisses <ulissessuporteti01@gmail.com>
2022-12-05 19:50:41 -0300

    adicionando funções para o dark e ligth mode - movendo main.js de '/js' para './src/js'

--- src/js/main.js
+++ src/js/main.js
@@ -49,3 +49,63 @@ const verificaInput = async () => {
 };
 
 verificaInput();
+
+// temas ligth/dark
+
+window.onload = () => {
+  if (localStorage.tema == "light") ligthMode();
+  else if (localStorage.tema == "dark") darkMode();
+  else null;
+};
+
+window.onloadeddata = () => {
+  if (localStorage.tema == "light") ligthMode();
+  else if (localStorage.tema == "dark") darkMode();
+  else null;
+};
+
+function ligthMode() {
+  selecionar("body").classList.remove("bg-dark");
+  selecionar(".navbar").classList.remove("navbar-dark");
+  selecionar(".navbar").classList.remove("bg-dark");
+  selecionar(".container-fluid").classList.remove("dark-mode");
+  selecionar("footer").classList.remove("dark-mode");
+  selecionar("footer").classList.add("bg-light");
+  selecionar("[form]").classList.remove("textareaCustom-dark");
+  selecionar("[form-textarea]").classList.add("bg-body");
+  selecionar("[form-textarea]").classList.remove("bg-dark");
+  selecionar("[container-textarea]").classList.remove(
+    "container-textarea-dark"
+  );
+  selecionar("[form-textarea]").classList.remove("bg-dark");
+  selecionar("[form-textarea]").classList.remove("textarea-dark");
+}
+
+function darkMode() {
+  selecionar("body").classList.add("bg-dark");
+  selecionar(".navbar").classList.add("navbar-dark");
+  selecionar(".navbar").classList.add("bg-dark");
+  selecionar(".container-fluid").classList.add("dark-mode");
+  selecionar("footer").classList.add("dark-mode");
+  selecionar("footer").classList.remove("bg-light");
+  selecionar("[form]").classList.add("textareaCustom-dark");
+  selecionar("[form-textarea]").classList.remove("bg-body");
+  selecionar("[form-textarea]").classList.add("bg-dark");
+  selecionar("[container-textarea]").classList.add("container-textarea-dark");
+  selecionar("[form-textarea]").classList.add("bg-dark");
+  selecionar("[form-textarea]").classList.add("textarea-dark");
+}
+
+const selecionar = (seletor) => document.querySelector(seletor);
+
+selecionar("[ligth]").onclick = () => {
+  localStorage.tema = "light";
+  console.log("let there be light!");
+  ligthMode();
+};
+
+selecionar("[dark]").onclick = () => {
+  localStorage.tema = "dark";
+  console.log("turn off the light, please!");
+  darkMode();
+};
